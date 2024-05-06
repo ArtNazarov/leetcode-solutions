685c7537ad47ec753263229d913adbff480f6805
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 20:25:51 +0500

    Time: 6 ms (10.01%) | Memory: 8 MB (57.27%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -5,44 +5,44 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        vector<int> x;
-        vector<int> y;
+ 
         string z = "";
-        for (int i = a.size() - 1; i != -1; i--) {
-            int value = int(a[i]) - int('0');
-            x.insert(x.end(), value);
-            /*  cout << value; */
-        };
-        // cout << endl;
-        for (int i = b.size() - 1; i != -1; i--) {
-            int value = int(b[i]) - int('0');
-            y.insert(y.end(), value);
-            /*  cout << value; */
-        };
+         
         // cout << endl;
         vector<int> s;
         size_t i = 0;
         size_t j = 0;
         int shift = 0;
-        while (i < x.size() && j < y.size()) {
-            int dig = (x[i] + y[i] + shift) % 2;
-            shift = (x[i] + y[i] + shift) / 2;
+        while (i < a.size() && j < b.size()) {
+            int ni = a.size()-i-1;
+            int nj = b.size()-j-1;
+            int x = a[ni] - '0';
+            int y = b[nj] - '0';
+            int s = x + y + shift;
+            int dig = s % 2;
+            shift = s >> 1;
             z.insert(z.begin(), '0'+dig);
             i++;
             j++;
         };
-        if ( j < y.size() ) {
-            while (j < y.size()) {
-            int dig = (y[j] + shift) % 2;
-            shift = (y[j] + shift) / 2;
-             z.insert(z.begin(), '0'+dig);
+        if ( j < b.size() ) {
+            while (j < b.size()) {
+                int nj = b.size()-j-1;
+                int y = b[nj] - '0';
+                int s = y + shift;
+                int dig = s % 2;
+                shift = s >> 1;
+                z.insert(z.begin(), '0'+dig);
 
             j++;
             };
-        } else if ( i < x.size() ) {
-            while (i < x.size()) {
-            int dig = (x[i] + shift) % 2;
-            shift = (x[i] + shift) / 2;
+        } else if ( i < a.size() ) {
+            while (i < a.size()) {
+                int ni = a.size() - i - 1;
+                int x = a[ni] - '0';
+                int s = x + shift;
+                int dig = s % 2;
+                shift =  s >> 1;
              z.insert(z.begin(), '0'+dig);
 
             i++;
