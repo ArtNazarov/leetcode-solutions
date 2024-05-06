a45821d50b9aaf4f62b407738fd6a9a8e37c0e45
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 19:42:13 +0500

    Time: 0 ms (100.00%) | Memory: 7.6 MB (55.89%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -4,10 +4,39 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-         stringstream ss(s);
-         string w = "";
-         while (ss >> w) {};
-         return w.size();
+         string& text = s;
+         const char delim = ' ';
+          
+         int result = 0;
+         int sizeCurrentToken = 0;
+         
+         for (int i=text.length()-1;i>=0;--i){
+             // if not delimeter add to token
+             
+             if (text[i]!=delim){
+                 sizeCurrentToken++;     
+             };
+              
+             
+
+             // if delimiter and token not empty
+             // add them to tokens list and 
+             // change token to empty string
+             if (text[i]==delim){
+                 if (sizeCurrentToken!=0){
+                     result = sizeCurrentToken;
+                     sizeCurrentToken = 0;
+                     break;
+                 };
+             };
+         };
+            // if not delimeter in first char
+
+             if (sizeCurrentToken!=0){
+                 result = sizeCurrentToken;
+             };
+
+        return   result;  
          
           
     }
