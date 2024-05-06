edc96989812075777f743f988faf2b797fd39ee1
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 21:41:41 +0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (23.87%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,19 +1,56 @@
+void computeLPSMap(string& pat, map<int, int>& lps)
+{
+    string_view s = pat;
+    int n = s.size();
+	 
+	for (int i=0; i<n; ++i)
+		for (int k=0; k<=i; ++k)
+			if (s.substr(0,k) == s.substr(i-k+1,k))
+				lps[i] = k;
+	 
+}
+
+
 #include <string>
 using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-      int ps = 0;
-      int sz1 = haystack.size();
-      int sz2 = needle.size();
-      if (sz2 > sz1) return -1;
-      
-      while (ps < sz1){
-           string s = haystack.substr(ps, sz2);
-           if (s == needle) return ps;
-           ps++;
-        };
-   
-      return -1;
+        string_view s1 = haystack;
+        string_view s2 = needle;
+        int m = s1.size();
+        int n = s2.size();
+        if (n > m) return -1;
+        map<int, int> lps;
+        computeLPSMap(needle, lps);
+        for(auto p : lps) cout << p.first << " " << p.second << endl;
+    int M = needle.size();
+    int N = haystack.size();
+ 
+     
+ 
+    int i = 0;  
+    int j = 0; 
+    while ((N - i) >= (M - j)) {
+        if (needle[j] == haystack[i]) {
+            j++;
+            i++;
+        }
+ 
+        if (j == M) {
+            return i - j;
+            j = lps[j - 1];
+        }
+ 
+         
+        else if (i < N && needle[j] != haystack[i]) {
+             
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    };
+    return -1;
     }
 };
