6fa3b42b73f4121735da9826a79941aaaea73bbe
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 00:43:06 +0500

    Time: 98 ms (40.57%) | Memory: 37.4 MB (35.12%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,7 +5,12 @@
 
 class Solution {
 public:
-  
+ 
+void clear_duplicates(vector<vector<int>> &v) {
+  std::sort(v.begin(), v.end());
+  auto last = std::unique(v.begin(), v.end());
+  v.erase(last, v.end());
+}
 
 vector<vector<int>> threeSum(vector<int> &nums) {
   map<vector<int>, int> f;
@@ -13,26 +18,22 @@ vector<vector<int>> threeSum(vector<int> &nums) {
   vector<vector<int>> res = {};
   std::sort(nums.begin(), nums.end());
   
-  size_t zeroes = std::count(nums.begin(), nums.end(), 0);
-  if (zeroes == nums.size()){
-    return {{0,0,0}};
-  };
+  
    
-   int sz =  nums.size();
   set<int> sums;
-  for (size_t i = 0; i < sz-2; i++) {
+  for (size_t i = 0; i < nums.size()-2; i++) {
+    if (i>0 && nums[i]==nums[i-1]) continue;
     int leftBorder = i + 1;
-    int rightBorder = sz-1;
+    int rightBorder = nums.size()-1;
     while (leftBorder < rightBorder){
         
         int sum = nums[i] + nums[leftBorder] + nums[rightBorder];
         
         if (sum < 0) {
-            while (leftBorder < rightBorder && nums[i] + nums[leftBorder] + nums[rightBorder]<0) leftBorder ++;
+            leftBorder ++;
         }
         else if (sum > 0){
-             while (leftBorder < rightBorder && nums[i] + nums[leftBorder] + nums[rightBorder]>0) rightBorder --;
-       
+            rightBorder --;
         }
         else {
            
