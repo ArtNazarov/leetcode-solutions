a9835264a58f78a55c1d5be129ed8e7bdc6dcc1c
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 12:07:39 +0500

    Time: 21 ms (5.37%) | Memory: 8.4 MB (32.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int temp = x;
+        int max_exp = 0;
+        while (temp > 0){
+            max_exp++;
+            temp = temp >> 1;
+            temp /= 5;
+        };
+        int mx = pow(10, max_exp-1);
+        int a = x;
+        int b = x;
+        int cnt = 0;
+        do {
+            int d1 = a;
+            if (mx != 0)  d1 = a / mx;
+            
+            int d2 = b % 10;
+            //cout << d1 << " " << d2 << endl;
+            if (d1 != d2) return false;
+            a  -= d1 * mx;
+            mx /= 10;
+            b /= 10;
+            cnt++;
+        } while (cnt<=max_exp);
+        return true;
+    }
+};
