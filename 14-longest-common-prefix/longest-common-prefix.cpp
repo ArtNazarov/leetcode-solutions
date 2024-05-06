916a4de60a924ff309433ac52da71cb9079a22dc
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 13:21:25 +0500

    Time: 3 ms (71.53%) | Memory: 10.8 MB (88.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,22 +4,23 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        map<int, set<char>> m;
-        int minSize = strs[0].size();
-        int i = 0;
-        for (const auto& s  : strs){
-                minSize = min((int)s.size(), minSize);
-                int j = 0;
-                for(auto ch : s){
-                    m[j].insert(ch);
-                    j++;
-                };    
-                i++;   
-        };
-         
+        const int MAX_L = 200;  
         string prefix = "";
-        for (int i=0;i<minSize;i++){
-           if (m[i].size()==1) prefix += *(m[i].begin()); else break;
+        for (int i=0;i<=MAX_L - 1;i++){
+            bool add_char = true;
+            if (i >= strs[0].size()) break;
+            char l = strs[0][i];
+            for (const auto& s : strs){
+                if (i >= s.size()) {add_char = false; break;};
+                add_char = add_char && s[i] == l;
+                if (!add_char) {break;};
+            }
+            if (add_char){
+                prefix += l;
+            } else
+            {
+                break;
+            };
         };
         return prefix;
     }
