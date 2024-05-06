773acfa0afa5529f80a68dd25d8ff02f9e8a71d1
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 19:32:55 +0500

    Time: 3 ms (70.71%) | Memory: 12.1 MB (45.21%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,8 +4,8 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
       
-      return std::distance(nums.begin(), std::lower_bound(nums.begin(),
-                nums.end(), target));;
+      return  abs(nums.begin() - std::lower_bound(nums.begin(),
+                nums.end(), target));
 
     }
 };
