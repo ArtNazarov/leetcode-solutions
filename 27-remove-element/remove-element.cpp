e921d762b58c3ac6a7b45c4e1135aad1dfe8ffdc
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-06 16:53:35 +0500

    Time: 0 ms (100.00%) | Memory: 10.8 MB (37.56%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,18 +4,19 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
       // get all numbers not equals val
-      vector<int> notEquals;
-       std::copy_if (
-                nums.begin(), 
-                nums.end(), 
-                std::back_inserter(notEquals), 
-                [ = ](int i){return i!=val;} );  
-       
-      int result = notEquals.size(); 
-      // changing first numbers to notEquals
-      for (int i=0;i<result;i++){
-        nums[i] = notEquals[i];
+      vector<int> v = nums;
+      vector<int> notEq;
+      sort(v.begin(), v.end());
+      auto lw = lower_bound(v.begin(), v.end(), val);
+      auto up = upper_bound(v.begin(), v.end(), val);
+      for(auto it = v.begin(); it != lw; it++){
+        notEq.push_back(*it);
       };
-      return result;
+      for(auto it = up; it != v.end(); it++){
+        notEq.push_back(*it);
+      };
+      int sz = notEq.size();
+      for(auto i=0;i<sz;i++) nums[i] = notEq[i];
+      return sz; 
     }
 };
