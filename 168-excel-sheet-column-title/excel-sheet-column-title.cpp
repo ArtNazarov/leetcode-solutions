12ca4d81e2394bc2aaf1dde1070794bdab403fe7
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 15:47:28 +0500

    Time: 2 ms (54.60%) | Memory: 7.1 MB (25.82%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
 string convertToTitle(int columnNumber) {
-  const int count = int('Z') - int('A') + 1;
+  const int count = 26;
   string s = "";
   while (columnNumber > 0) {
     columnNumber -= 1;
     int dig = columnNumber % count;
-    s.insert(0, string(1, char(int('A') + dig)));
+    s = char(int('A') + dig) + s;
     columnNumber /= count;
   }
   return s;
