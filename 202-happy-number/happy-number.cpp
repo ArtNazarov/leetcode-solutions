8f9f890c97b7c9e700f2d2d017077aafe3de30bc
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 18:36:32 +0500

    Time: 5 ms (19.52%) | Memory: 8.5 MB (8.02%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+bool isHappy(int n) {
+   bool flag = false; 
+   map<int, int> prev; // previous sums
+   while(!flag){
+  
+   if (1==n) {
+       flag = true;
+       break;
+   }
+   int sum = 0;
+  
+   int digit;
+   while (n != 0){
+       digit = n % 10;
+       // cout << "Digit " << digit << endl;
+       // cout << "N is " << n << endl;
+       sum += digit * digit;
+       // cout << sum << endl;
+       n = n >> 1;
+       n /= 5;
+      
+   };
+   
+      
+   n = sum;
+     
+     if (prev[n]==0){
+       // not found
+     prev[n] = 1;
+     } else {
+       // cycle!
+       // cout << "Cycle with " << n << endl;
+       flag = false;
+       break;
+     };
+     
+    // cout << n << endl;
+   };
+   return flag;
+}
+};
