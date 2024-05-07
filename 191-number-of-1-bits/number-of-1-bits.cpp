b3ef77789654d94a57537d07b91d956b79d4520a
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 17:31:52 +0500

    Time: 0 ms (100.00%) | Memory: 7.3 MB (51.30%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <iostream>
+
+class Solution {
+public:
+int hammingWeight(uint32_t n) {
+  int s = 0;
+  while (n > 0) {
+    if (n % 2 != 0)
+      s++;
+    n = n >> 1;
+  };
+  return s;
+}
+};
