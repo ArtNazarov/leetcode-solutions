2a82a32959942c1b033b3a439d9ac20dc77eb641
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 12:51:20 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.85%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,10 +4,10 @@ public:
         vector<int> first_row = {1};
         vector<int> second_row = {1, 1};
         vector<vector<int>> rows;
-        rows.insert(rows.end(), first_row);
-        rows.insert(rows.end(), second_row);
+        rows.push_back(first_row);
+        rows.push_back(second_row);
         for (int i=2; i<=numRows;i++){
-            rows.insert(rows.end(), vector<int>(i+1));
+            rows.push_back(vector<int>(i+1));
             rows[i][0] = 1;
             rows[i][rows[i].size()-1] = 1;
             for (int j=1; j<i; j++){
