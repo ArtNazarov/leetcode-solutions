10fb3a6884f41e83128694cc1c7cca7b0f7ab626
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 10:47:50 +0500

    Time: 66 ms (60.22%) | Memory: 29.1 MB (48.05%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -15,12 +15,12 @@ vector<vector<int>> threeSum(vector<int> &nums) {
   
   
    
-  
-  for (size_t i = 0; i < nums.size()-2; i++) {
+  int sz = nums.size();
+  for (size_t i = 0; i < sz - 2; i++) {
     if (nums[i]>0) break;
     if (i>0 && nums[i]==nums[i-1]) continue;
     int leftBorder = i + 1;
-    int rightBorder = nums.size()-1;
+    int rightBorder = sz-1;
     while (leftBorder < rightBorder){
         
         int sum = nums[i] + nums[leftBorder] + nums[rightBorder];
@@ -38,7 +38,7 @@ vector<vector<int>> threeSum(vector<int> &nums) {
             vector<int> v = {nums[i], nums[leftBorder], nums[rightBorder]};
             //f[v]++;
             //if (f[v]==1) 
-            res.push_back(v);
+            res.insert(res.end(), v);
             leftBorder = upper_bound(nums.begin(), nums.end(), nums[leftBorder]) - nums.begin();
             rightBorder = prev(lower_bound(nums.begin(), nums.end(), nums[rightBorder])) - nums.begin();
         
