8e63cb5755047ff186efea7b231128ffa74c5c06
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-07 20:26:41 +0500

    Time: 0 ms (100.00%) | Memory: 12 MB (5.87%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,48 +10,28 @@
  */
 class Solution {
 public:
-    ListNode* makeList(const vector<int>& v){
-    std::pair<ListNode*, ListNode*> list;
-    for (auto e : v){
-        insertToLinkedList(list, e);
-    };
-    return list.first;
-}
-   
-   void insertToLinkedList(std::pair<ListNode*, ListNode*>& list, int val){
-                // cout << "Insert " << val << endl;
-                ListNode* nxtNode = new ListNode(val);
-                if (list.first == nullptr){
-                    list.first = nxtNode;
-                    list.second = nxtNode;
-                    return;
-                };
-                list.second = list.first;
-                 
-                 while (list.second->next != nullptr) {
-                    list.second = list.second->next;
-                };
-                 
-                
-                list.second->next = nxtNode; 
-                
-                
-    }
-
-    vector<int> makeVector(ListNode* lst){
-        vector<int> v;
-        ListNode* current = lst;
-        while (current != nullptr){
-            v.push_back(current->val);
-            current = current->next;
-        }
-        return v;
-    }
- 
+  
     ListNode* reverseList(ListNode* head) {
-        vector<int> v = makeVector(head);
-        vector<int> rev;
-        ranges::copy(v.rbegin(), v.rend(), back_inserter(rev));
-        return makeList(rev);
-    }
+        ListNode* ptr = head;
+        stack<int> st;
+        while (ptr != nullptr){
+            st.push(ptr->val);
+            ptr = ptr->next;
+        };
+        ListNode* res = nullptr;
+        ListNode* head_res = nullptr;
+        while (!st.empty()){
+            //cout << st.top() << endl;
+            
+            if (res == nullptr) {
+                head_res = new ListNode(st.top());
+                res = head_res;
+            } else {
+                res->next = new ListNode(st.top());
+                res = res->next;
+            };
+            st.pop();
+        }
+        return head_res; 
+     }
 };
