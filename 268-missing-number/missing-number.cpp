8a06da37d863ef36fb28cf44c93f6fda8c57537f
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-08 10:57:17 +0500

    Time: 19 ms (23.59%) | Memory: 21 MB (20.92%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -7,14 +7,8 @@ public:
         sort(nums.begin(), nums.end());
         auto start = nums.begin();
         auto end = nums.end();
-        for (int x = 0; x <= sz / 2 ; x++){
-             
+        for (int x = 0; x <= sz ; x++){
             if (!binary_search(start, end, x)) return x;
-            if (!binary_search(start, end, sz-x)) return sz-x;
-            int r = rand() % (sz - r);
-            if (!binary_search(start, end, r)) return r;
-            if (!binary_search(start, end, sz-r)) return sz-r;
-          
         };
         return 0;
     }
