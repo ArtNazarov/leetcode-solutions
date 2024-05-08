6f3cc85e313119c5719f6433f9ec0ceeaa26914e
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-08 10:47:41 +0500

    Time: 19 ms (23.59%) | Memory: 21.1 MB (14.18%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,11 +4,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sz = nums.size();
-        int hsz = sz >> 1;
-        ranges::sort(nums);
+        sort(nums.begin(), nums.end());
         auto start = nums.begin();
         auto end = nums.end();
-        for (int x = 0; x <= hsz ; x++){
+        for (int x = 0; x <= sz >> 1 ; x++){
             if (!binary_search(start, end, x)) return x;
             if (!binary_search(start, end, sz-x)) return sz-x;
         };
