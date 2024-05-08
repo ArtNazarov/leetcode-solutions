3624ec22662568718c9898477b5310366822f833
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-08 17:24:09 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (48.85%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -5,8 +5,8 @@ public:
       int min_k = 1;
       int max_k = n; // n is target
       while (min_k <= max_k){
-        int maybe_k = min_k + (max_k - min_k) / 2; // compute mid point
-        long long sumP = (long)maybe_k * (long)(maybe_k + 1) >> 1; // f(k) - ariphmetic progression sum
+        int maybe_k = min_k + ( (max_k - min_k) >> 1 ); // compute mid point
+        long long sumP = ( (long)maybe_k * (long)(maybe_k + 1) ) >> 1; // f(k) - ariphmetic progression sum
         if (sumP == n) return maybe_k; // if exactly equal
         if (sumP < n) {
             min_k = maybe_k + 1;
