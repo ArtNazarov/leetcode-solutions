26e236d5c93b91747f2eead1859e44ec4e9c325f
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-08 10:34:14 +0500

    Time: 11 ms (37.65%) | Memory: 9.8 MB (19.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,11 +4,13 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size()!=t.size()) return false;
-        set<char> st1; map<int, int> f1;
-        set<char> st2; map<int, int> f2;
-        for(auto ch : s) { f1[ch]++; st1.insert(ch); };
-        for(auto ch : t) { f2[ch]++; st2.insert(ch); };
-        if (st1.size()!=st2.size()) return false;
+        map<int, int> f1;
+        map<int, int> f2;
+        int sz = s.size();
+        for(auto i=0;i<sz;i++){
+            f1[s[i]]++;
+            f2[t[i]]++;
+        }
         return f1 == f2;
     }
 };
