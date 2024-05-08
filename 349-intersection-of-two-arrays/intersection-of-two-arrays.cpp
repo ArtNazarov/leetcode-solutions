f5005b8c6c6dcba12144683fd34df7e3b2f1656c
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-08 14:13:11 +0500

    Time: 9 ms (15.76%) | Memory: 12.6 MB (80.15%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+         
+     
+     set<int> st;
+       sort(nums2.begin(), nums2.end());
+        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end());
+
+       for (auto e : nums1){
+           if (binary_search(nums2.begin(), nums2.end(), e))  st.insert(e);
+       };
+
+       for (auto e : nums2){
+
+           if (st.find(e)==st.end()){
+               st.erase(e);
+        };
+       };
+
+
+
+      vector<int> res;
+      for (auto key : st){
+          res.push_back(key);
+    };
+      return res;
+
+    }
+};
