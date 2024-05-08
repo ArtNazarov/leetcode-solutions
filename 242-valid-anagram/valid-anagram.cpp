484589d74706762b0f2231048c42cb66d6998950
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-08 10:27:24 +0500

    Time: 10 ms (45.36%) | Memory: 8.4 MB (96.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,16 +3,9 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<int, int> f;
-        if ( s.size()!=t.size() ) return false;
-        for(auto ch : s) f[ch]++;
-        for(auto ch : t) {
-            f[ch]--;
-            if (f[ch]<0) return false;
-        }
-        for(auto ch : s){
-            if (f[ch]>0) return false;
-        }
-        return true;
+        if (s.size()!=t.size()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return (s.compare(t)==0);
     }
 };
