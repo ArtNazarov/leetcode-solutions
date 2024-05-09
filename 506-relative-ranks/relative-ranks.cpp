2a2a17e3e6afcc26784ce896be5396275cbf38cf
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-09 10:50:09 +0500

    Time: 0 ms (100.00%) | Memory: 13.4 MB (94.15%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -3,10 +3,10 @@ public:
     vector<string> findRelativeRanks(vector<int>& score) {
       vector<size_t> ix(score.size());
       iota(ix.begin(), ix.end(), 0);
-      ranges::sort(ix.begin(), ix.end(), [&score](int i, int j){return score[i]>score[j];});
+      sort(ix.begin(), ix.end(), [&score](int i, int j){return score[i]>score[j];});
      // for (auto i : ix) cout << i << " "; cout << endl;
       vector<string> res(score.size());
-      int sz = ix.size();
+      size_t sz = ix.size();
       for (int i=0;i<sz;i++){
         if (i==0) {res[ix[i]]="Gold Medal";};
         if (i==1) {res[ix[i]]="Silver Medal";};
