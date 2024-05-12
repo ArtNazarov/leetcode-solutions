a5780911af800917d7dddcf830d54c8fc69bb1b4
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-12 11:25:43 +0500

    Time: 0 ms (100.00%) | Memory: 10.4 MB (14.83%) - LeetSync

---
+++ 2304-cells-in-a-range-on-an-excel-sheet/cells-in-a-range-on-an-excel-sheet.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<string> cellsInRange(string s) {
+        char col1 = s[0];
+        char col2 = s[3];
+        char row1 = s[1];
+        char row2 = s[4];
+        vector<string> v;
+        for(auto col=col1;col<=col2;col++){
+            for(auto row=row1;row<=row2;row++){
+                string x = "";
+                x += col;
+                x += row;
+                v.push_back(x);
+            };
+        };
+        return v;
+    }
+};
