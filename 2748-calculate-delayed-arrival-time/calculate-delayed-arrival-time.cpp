7ece63b434c37c3e75b5b8e1425a7286d324c4ee
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-12 13:59:36 +0500

    Time: 0 ms (100.00%) | Memory: 7.3 MB (46.91%) - LeetSync

---
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
@@ -0,0 +1,8 @@
+// using rest
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int x = ( arrivalTime + delayedTime ) % 24;
+        return x;
+    }
+};
