c840567150d1cbf69c5c801e07d1c089826ac1d9
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-05-13 14:08:59 +0500

    Time: 4 ms (38.70%) | Memory: 8.7 MB (60.81%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+#include <ranges>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int i=0; i<matrix.size();i++){
+            for(int j=i+1;j<matrix[0].size();j++){
+                swap(matrix[i][j], matrix[j][i]);
+            };
+            ranges::reverse(matrix[i]);
+        }
+    }
+};
